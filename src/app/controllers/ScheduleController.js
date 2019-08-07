1124540c780ebe66bfe37684dfaa9eab21f080a3
Robson da Motta <muitopoco@gmail.com>
2019-08-07 19:26:45 -0300

    adicionando controller e consulta de scheduler

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,33 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    }).catch();
+
+    if (!checkProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    }).catch();
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
